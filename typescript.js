50c8129f7b418bd055b59f47e9b5d0516392dcb2
Kasturi <kasturi.c.chiplunkar@gmail.com>
2024-11-21 23:59:21 +0530

    delete js file

--- typescript.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// let u = true;
-// u = "string" //Error: type string is not assignable parameter to type boolean
-// Math.round(u) //Argument of type 'boolean' is not assignable to parameter of type number.
-//To avoid below error : 
-let v = true;
-v = "string";
-Math.round(v);
